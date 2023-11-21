a43e6e0289493bb8bd4e64aedd20f8631d348fa7
kevin.edgar <kevin.edgar@09-05-0023-0220.ad-ins.com>
2023-11-21 17:35:45 +0700

    Push new update speed improvement

---
+++ Object Repository/OCR Testing/checkLog/button_ListCustomer.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_ListCustomer</name>
+   <tag></tag>
+   <elementGuidId>7ce6f056-4cc2-4764-b8d6-047ab4d48523</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;_0rif_mat-tab-content-0-0&quot;]/div/storage-bucket-details-objects/storage-objects-table/storage-drop-target/div[2]/storage-objects-breadcrumb/div/cfc-breadcrumbs/div/div/ol/li[6]/cfc-breadcrumb-link-item/div/a/span[2]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Scripts/OCR Testing/API Only/CheckLog/Script1699340197659.groovy
+++ Scripts/OCR Testing/API Only/CheckLog/Script1699340197659.groovy
@@ -13,57 +13,79 @@ import org.openqa.selenium.JavascriptExecutor
 'lakukan check log bucket standard'
 checkProcedure('Standard')
 
+'klik pada button list customer'
+WebUI.click(findTestObject('OCR Testing/checkLog/button_ListCustomer'))
+
+'refresh laman web'
+WebUI.refresh()
+
 'ganti fokus robot ke tab baru'
 WebUI.switchToWindowIndex(1)
 
 'lakukan check log bucket coldline'
 checkProcedure('Coldline')
 
-'navigasi ke url bucket coldline'
-WebUI.navigateToUrl(findTestData(ExcelPathLogin).getValue(8, 8))
+'klik pada button list customer'
+WebUI.click(findTestObject('OCR Testing/checkLog/button_ListCustomer'))
+
+'refresh laman web'
+WebUI.refresh()
+
+//'navigasi ke url bucket coldline'
+//WebUI.navigateToUrl(findTestData(ExcelPathLogin).getValue(8, 8))
 
 'ganti fokus robot ke tab standard'
 WebUI.switchToWindowIndex(0)
 
-'navigasi ke url bucket coldline'
-WebUI.navigateToUrl(findTestData(ExcelPathLogin).getValue(8, 7))
+//'navigasi ke url bucket standard'
+//WebUI.navigateToUrl(findTestData(ExcelPathLogin).getValue(8, 7))
 
 def checkProcedure(String bucketType) {
-	'input filter OCR'
-	WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
-		findTestData(ExcelPathLogin).getValue(8, 11))
 	
-	WebUI.delay(1)
-	
-	'klik pada objek paling atas'
-	WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
-	
-	'input filter OCR'
-	WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
-		TenantCode)
-	
-	WebUI.delay(1)
-	
-	'klik pada objek paling atas'
-	WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
-	
-	'input filter OCR'
-	WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
-		OCRType)
-	
-	WebUI.delay(1)
-	
-	'klik pada objek paling atas'
-	WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
-	
-	'input filter OCR'
-	WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
-		'ListCustomer')
-	
-	WebUI.delay(1)
-	
-	'klik pada objek paling atas'
-	WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
+	if (GlobalVariable.onlyFirstRun == 0) {
+		'input filter OCR'
+		WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
+			findTestData(ExcelPathLogin).getValue(8, 11))
+		
+		WebUI.delay(1)
+		
+		'klik pada objek paling atas'
+		WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
+		
+		'input filter OCR'
+		WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
+			TenantCode)
+		
+		WebUI.delay(1)
+		
+		'klik pada objek paling atas'
+		WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
+		
+		'input filter OCR'
+		WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
+			OCRType)
+		
+		WebUI.delay(1)
+		
+		'klik pada objek paling atas'
+		WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
+		
+		'input filter OCR'
+		WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
+			'ListCustomer')
+		
+		WebUI.delay(1)
+		
+		'klik pada objek paling atas'
+		WebUI.click(findTestObject('OCR Testing/checkLog/topResult'))
+		
+		'jika sudah coldline, maka tidak perlu input lagi'
+		if (bucketType == 'Coldline') {
+			
+			'ubah status bahwa first run sudah dilakukan'
+			GlobalVariable.onlyFirstRun = 1
+		}
+	}
 	
 	'input filter OCR'
 	WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
@@ -111,12 +133,13 @@ def checkProcedure(String bucketType) {
 		}
 		
 	} else {
-		'input filter OCR'
-		WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
-			TimeOCR)
-		
-		WebUI.delay(1)
-		
+		for (i = 0 ; i < 2; i++) {
+			'input filter OCR'
+			WebUI.setText(findTestObject('OCR Testing/checkLog/inputSearchFilter'),
+				TimeOCR)
+			
+			WebUI.delay(1)
+		}
 		verifyObjectPresent(bucketType, OCRType)
 	}
 }
@@ -130,13 +153,11 @@ def verifyObjectPresent(String bucketType, String inc) {
 				GlobalVariable.NumOfColumn, GlobalVariable.StatusFailed,
 					(findTestData(ExcelPathOCRTesting).getValue(GlobalVariable.NumOfColumn, 2) + ';') + 'Nama Log tidak sesuai pada folder halaman' + (inc) + ' di Bucket ' + bucketType)
 		}
+		
 	} else {
 		'tulis kondisi gagal'
 		CustomKeywords.'writeToExcel.WriteExcel.writeToExcelStatusReason'(sheet,
 			GlobalVariable.NumOfColumn, GlobalVariable.StatusFailed,
 				(findTestData(ExcelPathOCRTesting).getValue(GlobalVariable.NumOfColumn, 2) + ';') + 'Log Not Found di bucket ' + bucketType)
-	
 	}
-	
-//	WebUI.verifyElementPresent(findTestObject('OCR Testing/checkLog/topResultFinal'), 1, FailureHandling.OPTIONAL)
 }
